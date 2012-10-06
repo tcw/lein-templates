eba5cd925bc29d99cf327dcc369f0ad147e2cb8e
tcw <tcwgit@gmail.com>
2012-10-06 23:17:23 +0200

    added description

--- basic-speclj-template/project.clj
+++ basic-speclj-template/project.clj
@@ -1,6 +1,6 @@
-(defproject basic-speclj-template/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject basic-speclj-template/lein-template "0.1.0"
+  :description "A Leiningen newnew template for a basic leiningen project with speclj tests"
+  :url "http://github.com/tcw/lein-templates"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
--- basic-web-mh-template/project.clj
+++ basic-web-mh-template/project.clj
@@ -1,6 +1,6 @@
-(defproject basic-web-mh-template/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject basic-web-mh-template/lein-template "0.1.0"
+  :description "A Leiningen newnew template for a basic Leiningen web project"
+  :url "http://github.com/tcw/lein-templates"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
