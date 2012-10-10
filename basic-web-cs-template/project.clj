a60d3404168041e205baa849bc0218f0ec1ae787
tcw <tcwgit@gmail.com>
2012-10-10 22:47:53 +0200

    removed SNAPSHOT

--- basic-web-cs-template/project.clj
+++ basic-web-cs-template/project.clj
@@ -1,4 +1,4 @@
-(defproject basic-web-cs-template/lein-template "0.1.0-SNAPSHOT"
+(defproject basic-web-cs-template/lein-template "0.1.0"
   :description "A Leiningen newnew template for a basic Leiningen web project"
   :url "http://github.com/tcw/lein-templates"
   :license {:name "Eclipse Public License"
