3d8061733be476b3c15d4a149cd4a155e392d2f6
tcw <tcwgit@gmail.com>
2012-10-10 22:45:36 +0200

    added description

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #lein-templates
 
 A collection of Leiningen newnew templates.
+
+All templates have been created with the lein-create-template plugin.
--- basic-web-cs-template/project.clj
+++ basic-web-cs-template/project.clj
@@ -1,6 +1,6 @@
 (defproject basic-web-cs-template/lein-template "0.1.0-SNAPSHOT"
-  :description "Created with lein-create-template"
-  :url "http://example.com/FIXME"
+  :description "A Leiningen newnew template for a basic Leiningen web project"
+  :url "http://github.com/tcw/lein-templates"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
